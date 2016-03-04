036a9364b5cb71d0b66e89a853fef5a852b133c8
zpjgit <zpj.love@foxmail.com>
2016-03-04 15:53:08 +0800

    v3, Hardware access services IBinder Reflect 0004

--- app/src/main/java/com/zpj/app_0001_ssd/MainActivity.java
+++ app/src/main/java/com/zpj/app_0001_ssd/MainActivity.java
@@ -11,8 +11,12 @@ import android.widget.CheckBox;
 import android.widget.Toast;
 //import com.zpj.hardlibrary.*;
 import android.view.View;
-import android.os.ILedService;
-import android.os.ServiceManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+//import android.os.ILedService;
+//import android.os.ServiceManager;
+import android.os.IBinder;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,8 +27,9 @@ public class MainActivity extends ActionBarActivity {
     private CheckBox checkBoxLed2 = null;
     private CheckBox checkBoxLed3 = null;
     private CheckBox checkBoxLed4 = null;
-    private ILedService iLedService = null;
-
+    //private ILedService iLedService = null;
+    Object Proxy = null;
+    Method ledCtrl =  null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +38,25 @@ public class MainActivity extends ActionBarActivity {
 
         button = (Button) findViewById(R.id.BUTTON);
 
-        iLedService = ILedService.Stub.asInterface(ServiceManager.getService("led"));
+        try {
+            //iLedService = ILedService.Stub.asInterface(ServiceManager.getService("led"));
+            Method getService = Class.forName("android.os.ServiceManager").getMethod("getService", String.class);
+            IBinder ledService = (IBinder)getService.invoke(null, "led");
+            Method asInterface = Class.forName("android.os.ILedService$Stub").getMethod("asInterface", IBinder.class);
+            Proxy = asInterface.invoke(null, ledService);
+            ledCtrl = Class.forName("android.os.ILedService$Stub$Proxy").getMethod("ledCtrl", int.class, int.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
-        //HardControl.ledOpen();
+        //ledCtrl.invoke(Proxy, 0, 1);
+                //HardControl.ledOpen();
 
         checkBoxLed1 = (CheckBox) findViewById(R.id.LED1);
         checkBoxLed2 = (CheckBox) findViewById(R.id.LED2);
@@ -55,28 +76,38 @@ public class MainActivity extends ActionBarActivity {
                     checkBoxLed3.setChecked(true);
                     checkBoxLed4.setChecked(true);
 
+
                     try {
                         for (int i = 0; i < 4; i++) {
-                            iLedService.ledCtrl(i, 1);
+                            //iLedService.ledCtrl(i, 1);
+                            ledCtrl.invoke(Proxy, i, 1);
                         }
-                    } catch (RemoteException e) {
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
                         e.printStackTrace();
                     }
-                } else {
+
+                }
+                else {
                     button.setText("ALL ON");
                     checkBoxLed1.setChecked(false);
                     checkBoxLed2.setChecked(false);
                     checkBoxLed3.setChecked(false);
                     checkBoxLed4.setChecked(false);
 
+
                     try {
                         for (int i = 0; i < 4; i++) {
-                            iLedService.ledCtrl(i, 0);
-
+                            //iLedService.ledCtrl(i, 0);
+                            ledCtrl.invoke(Proxy, i, 0);
                         }
-                    } catch (RemoteException e) {
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
                         e.printStackTrace();
                     }
+
 
                 }
             }
@@ -87,6 +118,7 @@ public class MainActivity extends ActionBarActivity {
         // Is the view now checked?
         boolean checked = ((CheckBox) view).isChecked();
 
+
         try {
             // Check which checkbox was clicked
             switch(view.getId()) {
@@ -94,56 +126,67 @@ public class MainActivity extends ActionBarActivity {
                     if (checked) {
                         // Put some meat on the sandwich
                         Toast.makeText(getApplicationContext(), "LED1 on", Toast.LENGTH_SHORT).show();
-                        iLedService.ledCtrl(0, 1);
+                        //iLedService.ledCtrl(0, 1);
+                        ledCtrl.invoke(Proxy, 0, 1);
                     }
                     else {
                         // Remove the meat
                         Toast.makeText(getApplicationContext(), "LED1 off", Toast.LENGTH_SHORT).show();
-                        iLedService.ledCtrl(0, 0);
+                        //iLedService.ledCtrl(0, 0);
+                        ledCtrl.invoke(Proxy, 0, 0);
                     }
                     break;
                 case R.id.LED2:
                     if (checked) {
                         // Put some meat on the sandwich
                         Toast.makeText(getApplicationContext(), "LED2 on", Toast.LENGTH_SHORT).show();
-                        iLedService.ledCtrl(1, 1);
+                        //iLedService.ledCtrl(1, 1);
+                        ledCtrl.invoke(Proxy, 1, 1);
                     }
                     else {
                         // Remove the meat
                         Toast.makeText(getApplicationContext(), "LED2 off", Toast.LENGTH_SHORT).show();
-                        iLedService.ledCtrl(1, 0);
+                        //iLedService.ledCtrl(1, 0);
+                        ledCtrl.invoke(Proxy, 1, 0);
                     }
                     break;
                 case R.id.LED3:
                     if (checked) {
                         // Put some meat on the sandwich
                         Toast.makeText(getApplicationContext(), "LED3 on", Toast.LENGTH_SHORT).show();
-                        iLedService.ledCtrl(2, 1);
+                        //iLedService.ledCtrl(2, 1);
+                        ledCtrl.invoke(Proxy, 2, 1);
                     }
                     else {
                         // Remove the meat
                         Toast.makeText(getApplicationContext(), "LED3 off", Toast.LENGTH_SHORT).show();
-                        iLedService.ledCtrl(2, 0);
+                        //iLedService.ledCtrl(2, 0);
+                        ledCtrl.invoke(Proxy, 2, 0);
                     }
                     break;
                 case R.id.LED4:
                     if (checked) {
                         // Put some meat on the sandwich
                         Toast.makeText(getApplicationContext(), "LED4 on", Toast.LENGTH_SHORT).show();
-                        iLedService.ledCtrl(3, 1);
+                        //iLedService.ledCtrl(3, 1);
+                        ledCtrl.invoke(Proxy, 3, 1);
                     }
                     else {
                         // Remove the meat
                         Toast.makeText(getApplicationContext(), "LED4 off", Toast.LENGTH_SHORT).show();
-                        iLedService.ledCtrl(3, 0);
+                        //iLedService.ledCtrl(3, 0);
+                        ledCtrl.invoke(Proxy, 3, 0);
                     }
                     break;
 
                 // TODO: Veggie sandwich
             }
-        } catch (RemoteException e) {
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
+
     }
 
 
